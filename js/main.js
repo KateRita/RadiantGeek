592e4e6fbb9e7424ac538e65796cf03fc17c99eb
Kate Reading <katerita@gmail.com>
2015-01-02 15:40:17 -0500

    deleted button.js, added JQuery UI for easing, added Click Handler to navbar to handle scrollbar offset and easing to scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+
+  
+
 $(function() {
-	$('body').scrollspy({ target: '.navbar-list' })
+  
+  	var offset = 50;
+  	
+	// create scrollspy object & attach to body
+	$('body').scrollspy({ target: '.navbar-list', offset: offset });
+	
+	// click handler for navbar
+	var $root = $('html, body');
+	$('.navbar li a').click(function(event) {
+	    event.preventDefault();
+	    
+	    // offset top of section, animate scroll
+	    var href = $.attr(this, 'href');
+		$root.animate({ 
+    		scrollTop: $(href).offset().top - offset + 1
+		}, 500, "easeOutExpo", function () {
+  	  		window.location.hash = href;
+	 	});
+	});
+	
 })

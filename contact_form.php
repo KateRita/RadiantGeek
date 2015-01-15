98399cc9201dcf6e96e0628f1d325a50eb4d8805
Kate Reading <katerita@gmail.com>
2015-01-14 20:12:24 -0500

    removed extra validation code from php and added more headers

--- contact_form.php
+++ contact_form.php
@@ -6,21 +6,22 @@ if (!isset($_POST['submit'])|| $_POST["submit"] != "contact") {
    exit;
 }
 
-/*
-$email = preg_replace("([\r\n])", "", $email);
-
-$find = "/(content-type|bcc:|cc:)/i";
-if (preg_match($find, $name) || preg_match($find, $email) || preg_match($find, $url) || preg_match($find, $comments)) {
-   echo "<h1>Error</h1>\n
-      <p>No meta/header injections, please.</p>";
-   exit;
-}*/
-
 // get the posted data
 $name = $_POST["nameInput"];
 $email_address = $_POST["emailInput"];
 $message = $_POST["messageInput"];
 
+$email = preg_replace("([\r\n])", "", $email);
+
+$find = "/(content-type|bcc:|cc:)/i";
+if (preg_match($find, $name) || preg_match($find, $email_address) || preg_match($find, $message)) {
+   echo "<h1>Error</h1>\n
+      <p>No meta/header injections, please.</p>";
+   exit;
+}
+
+/*
+ * //Validating in Javascript before form submit
 // validate data input
 // check that a name was entered
 if (empty($name))
@@ -38,15 +39,19 @@ elseif (empty($message))
 //if there is an error, send the user back to the form
 if (isset($error)) {
     header("Location: index.html?e=".urlencode($error)); exit;
-}
+}*/
 
 // write the email content
 $email_content = "Name: $name\n";
 $email_content .= "Email Address: $email_address\n";
 $email_content .= "Message:\n\n$message";
-$headers = 'From: katereading@radiantgeek.com' . "\r\n" .
-   			'Reply-To: katereading@radiantgeek.com' . "\r\n" .
-   			'X-Mailer: PHP/' . phpversion();
+
+//build headers
+	$headers = 'From: RadiantGeek katereading@radiantgeek.com' . "\r\n" ;
+   	$headers .=	'Reply-To: ' . $email_address . "\r\n" ;
+   	$headers .= 'X-Mailer: PHP/' . phpversion();
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";   
 
 // send the email
 mail ("katereading@radiantgeek.com", "New Contact Message", $email_content, $headers);
